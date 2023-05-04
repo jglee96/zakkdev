eb84b285e911220ac62561dbe674ed5f27d740a0
jglee96 <zakklee96@gmail.com>
2023-05-04 23:21:41 +0900

    scaffold a new blog post article automatically with nx workspace generators

---
+++ tools/generators/new-article/index.ts
@@ -0,0 +1,24 @@
+import {
+  Tree,
+  formatFiles,
+  generateFiles,
+  joinPathFragments,
+  names,
+} from '@nrwl/devkit';
+
+interface NewAritlceSchemaOptions {
+  title: string;
+  author: string;
+  excerpt?: string;
+}
+
+export default async function (tree: Tree, schema: NewAritlceSchemaOptions) {
+  generateFiles(tree, joinPathFragments(__dirname, './files'), './_articles', {
+    title: schema.title,
+    author: schema.author,
+    excerpt: schema.excerpt || '',
+    normalizedTitle: names(schema.title).fileName,
+    creationDate: new Date().toISOString(),
+  });
+  await formatFiles(tree);
+}
