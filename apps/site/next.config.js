68e505c1253db0077b7f383c0949c44cffb08a0e
jglee96 <zakklee96@gmail.com>
2023-05-08 23:40:24 +0900

    Share React Components in Next.js Applications with Nx Workspace Libraries
    * JIT mode를 사용하면 purge에 외부 컴포넌트를 import하는 패키지를
    명시해주어야 tailwind css가
    적용됨.(https://github.com/tailwindlabs/tailwindcss/discussions/6056)
    * nx에서는 createGlobPatternsForDependencies 를 통해 자동으로 처리

--- apps/site/next.config.js
+++ apps/site/next.config.js
@@ -1,7 +1,7 @@
 //@ts-check
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const { withNx } = require("@nrwl/next/plugins/with-nx");
+const { withNx } = require('@nrwl/next/plugins/with-nx');
 
 /**
  * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
@@ -10,7 +10,7 @@ const nextConfig = {
   nx: {
     // Set this to true if you would like to use SVGR
     // See: https://github.com/gregberge/svgr
-    svgr: false,
+    svgr: true,
   },
 };
 
