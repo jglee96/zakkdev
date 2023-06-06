055eff950f7708830b58c073711f5c3ae94fdddb
jglee96 <zakklee96@gmail.com>
2023-06-06 22:33:04 +0900

    fix: PostCard test 수정

--- apps/zakkdev/components/PostCard/PostCard.spec.tsx
+++ apps/zakkdev/components/PostCard/PostCard.spec.tsx
@@ -4,7 +4,16 @@ import PostCard from './PostCard';
 
 describe('PostCard', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<PostCard />);
+    const { baseElement } = render(
+      <PostCard
+        fileName="Acronym Naming"
+        frontMatter={{
+          title: 'Acronym Naming',
+          date: '2023-03-24T13:55:57.000Z',
+          author: { name: 'zakklee' },
+        }}
+      />
+    );
     expect(baseElement).toBeTruthy();
   });
 });
