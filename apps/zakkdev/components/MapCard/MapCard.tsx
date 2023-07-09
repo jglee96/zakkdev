6a082b0854efcaa7d4d5eedfaaea7f912afa72f9
jglee96 <zakklee96@gmail.com>
2023-07-09 22:35:59 +0900

    fix: map card 수정
    * map title이 보이지 않는 오류 수정
    * content 높이 고정, ellipsis 적용

--- apps/zakkdev/components/MapCard/MapCard.tsx
+++ apps/zakkdev/components/MapCard/MapCard.tsx
@@ -1,4 +1,10 @@
-import { Card, CardContent, CardFooter, CardHeader } from '@zakkdev/ui';
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@zakkdev/ui';
 import { MapCode } from '@zakkdev/types';
 import { IssueBadge } from '@zakkdev/ui';
 import Link from 'next/link';
@@ -8,17 +14,18 @@ interface Props {
   frontMatter: MapCode;
 }
 const MapCard = ({ fileName, frontMatter }: Props) => {
+  console.log(frontMatter);
   return (
     <Card
       key={fileName}
-      className="w-96 h-44 transform transition duration-150 hover:cursor-pointer hover:scale-105 hover:-translate-x-1 hover:-translate-y-1"
+      className="w-96 transform transition duration-150 hover:cursor-pointer hover:scale-105 hover:-translate-x-1 hover:-translate-y-1"
     >
       <Link href={`map/${fileName}`}>
         <CardHeader>
-          <title>{frontMatter?.title}</title>
+          <CardTitle className="m-0">{frontMatter?.title}</CardTitle>
         </CardHeader>
-        <CardContent>
-          <p>{frontMatter?.excerpt}</p>
+        <CardContent className="text-ellipsis h-20">
+          {frontMatter?.excerpt}
         </CardContent>
         <CardFooter>
           <IssueBadge status={frontMatter?.status} />
