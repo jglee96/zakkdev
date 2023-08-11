c9101f2b5060dad8f0a669a141bc263a67d0a054
jglee96 <zakklee96@gmail.com>
2023-08-11 19:41:57 +0900

    fix: blog post card 수정
    * post card에서 이미지 제거

--- apps/zakkdev/components/PostCard/PostCard.tsx
+++ apps/zakkdev/components/PostCard/PostCard.tsx
@@ -1,5 +1,9 @@
-import { Card, CardContent, CardFooter } from '@zakkdev/ui';
-import Image from 'next/image';
+import {
+  Card,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@zakkdev/ui';
 import { Post } from '@zakkdev/types';
 import dayjs from 'dayjs';
 import Link from 'next/link';
@@ -12,20 +16,14 @@ const PostCard = ({ fileName, frontMatter }: Props) => {
   return (
     <Card
       key={fileName}
-      className="w-64 h-72 transform transition duration-150 hover:cursor-pointer hover:scale-105 hover:-translate-x-1 hover:-translate-y-1"
+      className="w-72 transform transition duration-150 hover:cursor-pointer hover:scale-105 hover:-translate-x-1 hover:-translate-y-1"
     >
       <Link href={`blog/${fileName}`}>
-        <CardContent className="pt-6">
-          <Image
-            className="rounded"
-            width={250}
-            height={180}
-            src={`https://picsum.photos/seed/${fileName}/250/180`}
-            alt={fileName}
-          />
-        </CardContent>
+        <CardHeader>
+          <CardTitle>{frontMatter.title}</CardTitle>
+        </CardHeader>
+        {/* <CardContent className="h-20">{frontMatter.excerpt}</CardContent> */}
         <CardFooter className="flex flex-col items-start">
-          <p className="text-base font-bold">{frontMatter?.title}</p>
           <p className="text-sm">
             {dayjs(frontMatter?.date).format('YYYY.MM.DD')}
           </p>
