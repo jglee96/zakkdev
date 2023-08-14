62f2876641fb99307a86b44e38a6cceb8bf13ceb
jglee96 <zakklee96@gmail.com>
2023-08-14 20:19:12 +0900

    add: 파일 업로드 기능 구현
    * storage RLS 설정하여 파일 업로드 기능 구현
    * select policy 추가하여 목록 볼 수 있도록 수정

--- apps/zakkdev/app/manage/components/BlogUpload.tsx
+++ apps/zakkdev/app/manage/components/BlogUpload.tsx
@@ -1,19 +1,44 @@
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
 import { Card, CardHeader, CardTitle, CardContent, Button } from '@zakkdev/ui';
+import { FormEvent, useState } from 'react';
 
 export default function BlogUpload() {
+  const [files, setFiles] = useState<File[]>([]);
+
+  const supabase = createClientComponentClient();
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const uploadPromise = files.map(async (file) => {
+      return await supabase.storage
+        .from('articles')
+        .upload(`posts/${file.name}`, file, {
+          cacheControl: '3600',
+          upsert: false,
+        });
+    });
+
+    await Promise.all(uploadPromise);
+  };
   return (
     <Card className="w-96">
       <CardHeader>
         <CardTitle>BLOG 게시물 업로드</CardTitle>
       </CardHeader>
       <CardContent>
-        <form
-          onSubmit={(e) => {
-            console.log(e);
-          }}
-        >
+        <form onSubmit={handleSubmit}>
           <div className="flex flex-col">
-            <input className="block" type="file" />
+            <input
+              className="block"
+              type="file"
+              onChange={(e) => {
+                const list = e.target.files;
+                if (list === null) return;
+
+                setFiles([...files, ...list]);
+              }}
+            />
+            {files.map((file) => file.name)}
             <Button
               className="flex break-keep w-fit"
               type="submit"
