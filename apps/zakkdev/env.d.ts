75abee8124fa8ba0c2dc5fcfbeaa388ae6549aa8
jglee96 <zakklee96@gmail.com>
2024-03-23 21:03:52 +0900

    init: monorepo 제거 및 재시작

--- apps/zakkdev/env.d.ts
+++
@@ -1,7 +0,0 @@
-declare namespace NodeJS {
-  interface ProcessEnv {
-    NEXT_PUBLIC_NAVER_MAP_CLIENT_ID: string;
-    NEXT_PUBLIC_SUPABASE_URL: string;
-    NEXT_PUBLIC_SUPABASE_ANON_KEY: string;
-  }
-}
