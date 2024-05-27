9c313eabb9d9f9a7040280478ef2d96f5b99e209
jglee96 <zakklee96@gmail.com>
2024-05-28 00:26:25 +0900

    fix: snapping polygon 프로젝트 iframe으로 변경
    * navermap-snap repo 구현 사항을 그대로 보여주기 위해 iframe으로 변경

--- app/projects/snapping-polygon-with-naver-maps-api/page.tsx
+++ app/projects/snapping-polygon-with-naver-maps-api/page.tsx
@@ -8,53 +8,11 @@ import { InPolygon, OutPolygon, Polygon } from "./shape";
 export default function Page() {
   const mapRef = useRef<naver.maps.Map | null>(null);
   return (
-    <>
-      <Flex columnGap="md" pb="md">
-        <Button
-          onClick={() =>
-            new Polygon(mapRef.current!, {
-              fillColor: "blue",
-              fillOpacity: 0.3,
-              strokeColor: "blue",
-              strokeWeight: 3,
-              strokeStyle: "shortdashdotdot",
-            })
-          }
-        >
-          Polygon
-        </Button>
-        <Button
-          onClick={() =>
-            new InPolygon(mapRef.current!, {
-              fillColor: "orange",
-              fillOpacity: 0.3,
-              strokeColor: "orange",
-              strokeWeight: 1,
-            })
-          }
-        >
-          InPolygon
-        </Button>
-        <Button
-          onClick={() =>
-            new OutPolygon(mapRef.current!, {
-              fillColor: "green",
-              fillOpacity: 0.3,
-              strokeColor: "green",
-              strokeWeight: 1,
-            })
-          }
-        >
-          OutPolygon
-        </Button>
-      </Flex>
-      <div id="map" style={{ width: "100%", height: "80dvh" }} />
-      <Script
-        src={`https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${process.env.NEXT_PUBLIC_NAVER_MAP_CLIENT_ID}&submodules=drawing`}
-        onReady={() => {
-          mapRef.current = new naver.maps.Map("map");
-        }}
-      />
-    </>
+    <iframe
+      src="https://navermap-snap.vercel.app/"
+      width="100%"
+      height="100%"
+      style={{ border: 0 }}
+    />
   );
 }
