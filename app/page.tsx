ba01ad1e37737c41e59ebc201ffd788b0b8514db
jglee96 <zakklee96@gmail.com>
2025-05-05 16:27:49 +0900

    feat: 홈 페이지 내용 업데이트 및 포스트 제목 변경
    - 기존의 포스트 제목을 'zakklee'로 변경
    - 자기소개 및 블로그 목적에 대한 내용을 추가하여 사용자 경험 개선

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,6 @@ import { getDocPageMetas } from "@/utils/affine/reader";
 import Link from "next/link";
 import dayjs from "dayjs";
 
-interface Post {
-  title: string;
-  excerpt: string;
-  date: string;
-}
-
 async function getLatestPosts() {
   const metas = await getDocPageMetas();
 
@@ -22,8 +16,18 @@ export default async function Home() {
 
   return (
     <>
-      <Title>{RESUME_DATA.name}</Title>
-      <Text className={classes.summary}>{RESUME_DATA.summary}</Text>
+      <Title>zakklee</Title>
+      <Space h="md" />
+      <Text className={classes.summary} maw={840}>
+        안녕하세요, 개발자 이종건입니다.
+        <br />
+        <br />웹 개발을 중심으로 더 나은 사용자 경험과 효율적인 코드를 고민하며
+        다양한 프로젝트를 진행해 왔습니다. 이 블로그는 작업 중 마주한 문제와
+        해결 과정을 기록하고, 유용한 기술을 공유하기 위해 만들어졌습니다.
+        <br />
+        <br />
+        방문해주셔서 감사하고, 읽는 분들께 작은 도움이 되길 바랍니다.
+      </Text>
       <Space h="xl" />
       <Title>Lastest Posts</Title>
       {posts?.map(({ title, id, created, excerpt }) => (
