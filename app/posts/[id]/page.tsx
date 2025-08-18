5e379c6e878d120119afe50fb80d8922cf5a5c7a
이종건 <zakklee96@gmail.com>
2025-08-18 23:38:31 +0900

    Refact/upgrade package nextjs 15 (#29)
    * refact: upgrade nextjs to 15 from 14 by codemod
    
    * refact: package 마이그레이션
    
    - affine 설정

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -1,5 +1,5 @@
 import { Title } from "@mantine/core";
-import { compileMDX } from "next-mdx-remote/rsc";
+import { evaluate } from "next-mdx-remote-client/rsc";
 import remarkGfm from "remark-gfm";
 import { Comment } from "@/components/comment";
 import { getDocMarkdown } from "@/utils/affine/reader";
@@ -7,11 +7,12 @@ import { getDocMarkdown } from "@/utils/affine/reader";
 import "@/themes/prism-laserwave.css";
 import "@/themes/markdown.css";
 
-export default async function Page({ params }: { params: { id: string } }) {
+export default async function Page(props: { params: Promise<{ id: string }> }) {
+  const params = await props.params;
   const doc = await getDocMarkdown(params.id);
 
   if (doc === null) return <></>;
-  const { content } = await compileMDX({
+  const { content } = await evaluate({
     source: doc.md,
     options: {
       mdxOptions: { remarkPlugins: [remarkGfm] },
