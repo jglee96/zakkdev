1653dbd9ca926d6b2d7ced1c89443551c16c542f
jglee96 <zakklee96@gmail.com>
2025-10-09 16:03:47 +0900

    feat: MDX 이미지 컴포넌트 추가 및 콘텐츠 정렬 개선
    - MDX 컴포넌트에 Next.js의 이미지 컴포넌트를 사용하여 이미지 렌더링 방식을 개선
    - 콘텐츠 내 이미지 태그를 Mantine의 Image 컴포넌트로 변경하고, 중앙 정렬을 적용하여 가독성 향상

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,6 +1,10 @@
 import type { MDXComponents } from "mdx/types";
+import NextImage from "next/image";
+import { Image } from "@mantine/core";
 
-const components: MDXComponents = {};
+const components: MDXComponents = {
+  img: (props) => <Image component={NextImage} alt={props.alt} {...props} />,
+};
 
 export function useMDXComponents(): MDXComponents {
   return components;
